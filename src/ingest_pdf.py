45f2f07607934abf2339f8cb29b8cd316f570dd1
Vishal Sinha <ext-vishal.sinha@lahitapiola.fi>
2026-01-07 03:31:37 +0200

    Update ingest_pdf.py

--- src/ingest_pdf.py
+++ src/ingest_pdf.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 #from langchain.document_loaders import PyPDFLoader
 #from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
